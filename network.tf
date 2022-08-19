9d085239fb77a7426de4375806b5f57380b5adf9
gladson1996 <gladson.oliver@crayon.com>
2022-08-19 10:01:50 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,12 @@
+resource "azurerm_resource_group" "example" {
+  name     = "my-resources"
+  location = "West Europe"
+}
+
+
 module "network" {
   source  = "app.terraform.io/Glad_Org/network/azurerm"
   version = "3.5.0"
   # insert required variables here
+
 }
